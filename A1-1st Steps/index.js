ad4369cae6483d3d2893a861b6404956afcf1578
Gilbert <84069358+gilluscode@users.noreply.github.com>
2022-05-02 12:45:18 +0200

    1st Steps with Scrimba
    HTML Code and a simple UL in React

---
+++ A1-1st Steps/index.js
@@ -0,0 +1,11 @@
+// ReactDOM availble here. render(displays elements for me)
+// ReactDOM.render(<h2>I am using React!</h2>, document.getElementById('root')); //its appending this h2. ReactDom comes from the library.
+
+ReactDOM.render(
+    <ul>
+    <li> Task Handler </li>
+    <li> Task One </li>
+    <li> Taske Two </li>
+    </ul>, 
+    document.getElementById('root')
+); 
